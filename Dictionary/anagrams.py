85ba8e2ae64cc5a5f6802fa9ed1313fbd1da655b
John <John Saylor>
2021-04-17 01:33:52 -0400

    anagrams

---
+++ Dictionary/anagrams.py
@@ -0,0 +1,31 @@
+def sherlockAndAnagrams(s):
+    result = 0
+    for sublength in range(1, len(s)):
+        counts = {}
+        i = 0
+        while i + sublength <= len(s):
+            substring = str(sorted(list(s[i:i+sublength])))
+            if substring in counts:
+                result += counts[substring]
+                counts[substring] += 1
+            else:
+                counts[substring] = 1
+            i += 1
+
+    return result
+
+
+def permutations(n):
+    return factorial(n) / (2 * factorial(n - 2))
+
+
+def factorial(n):
+    if n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+
+res = sherlockAndAnagrams("mom")
+print(res)
+# res = factorial(4)
+# print(res)
